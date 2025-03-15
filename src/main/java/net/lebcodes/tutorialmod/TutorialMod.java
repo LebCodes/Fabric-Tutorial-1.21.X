6a7ff20e52e481843c4c2d93ca64960b31a14605
LebCodes <ibarracake@gmail.com>
2025-03-14 18:04:42 -0700

    added raw pink garnet block, pink garnet block, and item groups

--- src/main/java/net/lebcodes/tutorialmod/TutorialMod.java
+++ src/main/java/net/lebcodes/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.lebcodes.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lebcodes.tutorialmod.block.ModBlocks;
+import net.lebcodes.tutorialmod.item.ModItemGroups;
 import net.lebcodes.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,9 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
