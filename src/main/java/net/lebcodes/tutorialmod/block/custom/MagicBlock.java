46a586076cd924a2c845ccef6127df7563e86ec4
LebCodes <ibarracake@gmail.com>
2025-03-16 14:43:46 -0700

    added a few custom tooltips

--- src/main/java/net/lebcodes/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/lebcodes/tutorialmod/block/custom/MagicBlock.java
@@ -6,14 +6,19 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 //hover over 'Block' and press control + h to see all vanilla blocks and their functionality
 public class MagicBlock extends Block {
@@ -36,5 +41,11 @@ public class MagicBlock extends Block {
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.magic_block.tooltip"));
+        super.appendTooltip(stack, context, tooltip, options);
     }
 }
