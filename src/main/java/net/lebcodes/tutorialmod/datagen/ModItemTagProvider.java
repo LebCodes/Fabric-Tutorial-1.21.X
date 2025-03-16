507c049e596a3e1de0cda1d8773acc0d62071ff1
LebCodes <ibarracake@gmail.com>
2025-03-16 16:11:32 -0700

    added data generation so now i dont have to add each fuckass json file

---
+++ src/main/java/net/lebcodes/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,27 @@
+package net.lebcodes.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.lebcodes.tutorialmod.item.ModItems;
+import net.lebcodes.tutorialmod.util.ModTags;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.PINK_GARNET)
+                .add(ModItems.RAW_PINK_GARNET)
+                .add(Items.COAL) //if doesnt work try Items from the tag
+                .add(Items.STICK)
+                .add(Items.APPLE);
+    }
+}
