3f45aaa81d07dc877fa8515cd1514b2f963b1d35
LebCodes <ibarracake@gmail.com>
2025-03-16 17:02:10 -0700

    added pink garnet non-block blocks (stairs, slabs, buttons, pressure plates, fences, fence gates, walls, doors, trapdoors)

--- src/main/java/net/lebcodes/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/lebcodes/tutorialmod/datagen/ModRecipeProvider.java
@@ -48,5 +48,6 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .input(ModBlocks.MAGIC_BLOCK)
                 .criterion(hasItem(ModBlocks.MAGIC_BLOCK), conditionsFromItem(ModBlocks.MAGIC_BLOCK))
                 .offerTo(recipeExporter, Identifier.of(TutorialMod.MOD_ID, "raw_pink_garnet_from_magic_block"));
+
     }
 }
