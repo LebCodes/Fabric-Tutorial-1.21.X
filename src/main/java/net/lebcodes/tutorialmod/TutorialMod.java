2f574e89a099f0f92489720de2de262d850a2377
LebCodes <ibarracake@gmail.com>
2025-03-17 17:12:23 -0700

    added a new function to chisel (used custom nbt data components)

--- src/main/java/net/lebcodes/tutorialmod/TutorialMod.java
+++ src/main/java/net/lebcodes/tutorialmod/TutorialMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.lebcodes.tutorialmod.block.ModBlocks;
+import net.lebcodes.tutorialmod.component.ModDataComponentTypes;
 import net.lebcodes.tutorialmod.item.ModItemGroups;
 import net.lebcodes.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
@@ -19,6 +20,8 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		ModDataComponentTypes.registerDataComponentTypes();
 
 		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 
--- src/main/java/net/lebcodes/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/net/lebcodes/tutorialmod/item/custom/ChiselItem.java
@@ -1,6 +1,7 @@
 package net.lebcodes.tutorialmod.item.custom;
 
 import net.lebcodes.tutorialmod.block.ModBlocks;
+import net.lebcodes.tutorialmod.component.ModDataComponentTypes;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.screen.Screen;
@@ -48,6 +49,8 @@ public class ChiselItem extends Item {
                         item -> context.getPlayer().sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
                 //if i get error change item
                 world.playSound(null, context.getBlockPos(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS);
+
+                context.getStack().set(ModDataComponentTypes.COORDINATES, context.getBlockPos());
             }
         }
 
@@ -60,6 +63,10 @@ public class ChiselItem extends Item {
             tooltip.add(Text.translatable("tooltip.tutorialmod.chisel.shift_down"));
         } else {
             tooltip.add(Text.translatable("tooltip.tutorialmod.chisel"));
+        }
+
+        if(stack.get(ModDataComponentTypes.COORDINATES) != null) {
+            tooltip.add(Text.literal("Last Block Changed at " + stack.get(ModDataComponentTypes.COORDINATES)));
         }
 
         super.appendTooltip(stack, context, tooltip, type);
