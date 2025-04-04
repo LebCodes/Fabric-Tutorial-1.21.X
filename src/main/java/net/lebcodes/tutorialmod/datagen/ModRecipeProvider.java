20b61279885200e7d66903c59d1f8400919f8200
LebCodes <ibarracake@gmail.com>
2025-04-03 19:44:13 -0700

    added pink garnet horse armor (custom horse armor)

--- src/main/java/net/lebcodes/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/lebcodes/tutorialmod/datagen/ModRecipeProvider.java
@@ -50,7 +50,7 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .criterion(hasItem(ModBlocks.MAGIC_BLOCK), conditionsFromItem(ModBlocks.MAGIC_BLOCK))
                 .offerTo(recipeExporter, Identifier.of(TutorialMod.MOD_ID, "raw_pink_garnet_from_magic_block"));
 
-
+        offerSmithingTrimRecipe(recipeExporter, ModItems.LEBSENSE_SMITHING_TEMPLATE, Identifier.of(TutorialMod.MOD_ID, "lebsense"));
         //create a helper method for tool recipes
     }
 }
--- src/main/java/net/lebcodes/tutorialmod/item/ModItemGroups.java
+++ src/main/java/net/lebcodes/tutorialmod/item/ModItemGroups.java
@@ -38,6 +38,8 @@ public class ModItemGroups {
                         entries.add(ModItems.PINK_GARNET_BOOTS);
 
                         entries.add(ModItems.PINK_GARNET_HORSE_ARMOR);
+
+                        entries.add(ModItems.LEBSENSE_SMITHING_TEMPLATE);
                     }).build());
 
     public static final ItemGroup PINK_GARNET_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
