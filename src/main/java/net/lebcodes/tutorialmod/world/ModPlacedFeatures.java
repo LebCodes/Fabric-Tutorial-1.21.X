2af788b386af299dd9d9d3c3e0482aa2e0eb9e48
LebCodes <ibarracake@gmail.com>
2025-04-24 15:02:46 -0700

    added custom tree generation

--- src/main/java/net/lebcodes/tutorialmod/world/ModPlacedFeatures.java
+++ src/main/java/net/lebcodes/tutorialmod/world/ModPlacedFeatures.java
@@ -1,16 +1,14 @@
 package net.lebcodes.tutorialmod.world;
 
 import net.lebcodes.tutorialmod.TutorialMod;
+import net.lebcodes.tutorialmod.block.ModBlocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.FeatureConfig;
-import net.minecraft.world.gen.feature.PlacedFeature;
+import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
 import net.minecraft.world.gen.placementmodifier.PlacementModifier;
 
@@ -20,6 +18,8 @@ public class ModPlacedFeatures {
     public static final RegistryKey<PlacedFeature> PINK_GARNET_ORE_PLACED_KEY = registryKey("pink_garnet_ore_placed");
     public static final RegistryKey<PlacedFeature> NETHER_PINK_GARNET_ORE_PLACED_KEY = registryKey("nether_pink_garnet_ore_placed");
     public static final RegistryKey<PlacedFeature> END_PINK_GARNET_ORE_PLACED_KEY = registryKey("end_pink_garnet_ore_placed");
+
+    public static final RegistryKey<PlacedFeature> DRIFTWOOD_PLACED_KEY = registryKey("driftwood_placed");
 
     public static void bootStrap(Registerable<PlacedFeature> context) {
         var configuredFeatures = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
@@ -35,6 +35,10 @@ public class ModPlacedFeatures {
         register(context, END_PINK_GARNET_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_PINK_GARNET_ORE_KEY),
                 ModOrePlacement.modifiersWithCount(14,
                         HeightRangePlacementModifier.uniform(YOffset.fixed(-80), YOffset.fixed(80))));
+
+        register(context, DRIFTWOOD_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.DRIFTWOOD_KEY),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                        PlacedFeatures.createCountExtraModifier(2, 0.1f, 2), ModBlocks.DRIFTWOOD_SAPLING));
     }
 
     public static RegistryKey<PlacedFeature> registryKey(String name) {
--- src/main/java/net/lebcodes/tutorialmod/world/gen/ModWorldGeneration.java
+++ src/main/java/net/lebcodes/tutorialmod/world/gen/ModWorldGeneration.java
@@ -2,6 +2,8 @@ package net.lebcodes.tutorialmod.world.gen;
 
 public class ModWorldGeneration {
     public static void generateModWorldGen() {
+        //keep it in specific order (might be important)
         ModOreGeneration.generateOres();
+        ModTreeGeneration.generateTrees();
     }
 }
