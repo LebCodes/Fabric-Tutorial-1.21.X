29b08d6bdeaf75ed8eea6ff30617500ba30c5090
LebCodes <ibarracake@gmail.com>
2025-05-13 09:00:18 -0700

    added mantis spawns (entity spawns)

--- src/main/java/net/lebcodes/tutorialmod/world/gen/ModWorldGeneration.java
+++ src/main/java/net/lebcodes/tutorialmod/world/gen/ModWorldGeneration.java
@@ -8,5 +8,7 @@ public class ModWorldGeneration {
         ModOreGeneration.generateOres();
         ModTreeGeneration.generateTrees();
         ModBushGeneration.generateBushes();
+
+        ModEntitySpawns.addSpawns();
     }
 }
